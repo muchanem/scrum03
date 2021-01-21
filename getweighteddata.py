d962f144e8ddd530bab336f55c58560a6e9d14a7
Mark Muchane <mark@pztron.io>
2021-01-21 09:40:56 -0500

    data for regression code

--- getweighteddata.py
+++ getweighteddata.py
@@ -20,32 +20,32 @@ def get_as_dataframe(stocks):
     return dfs
 
 def send_as_dataframe(dfs):
-    for stock, df in dfs:
+    for stock, df in dfs.items():
         df.to_dict('records')
         for adict in df:
             db[stock].update_one({'_id': adict['_id']}, {'$set': adict})
 
 def add_weights(dfs):
-    for stock, df in dfs:
+    for stock, df in dfs.items():
        df["weight"] = ((df["likes"] + df["followers"] * df["retweets"]) + 1)
        #dfs[stock] = df
     return dfs
 
 def convert_times(dfs):
-    for stock, df in dfs:
+    for stock, df in dfs.items():
         df["date"] = np.vectorize(datetime.datetime.strptime)(df["date"], "%a %b %d %H:%M:%S %z %Y")
         #dfs[stock] = df
     return dfs
 def weighted_average(vals, weights):
     return np.average(vals, weights=weights)
 def add_weighted_sentiment(final_dfs, dfs):
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         for day in df.itertuples():
             filtered = dfs[stock].loc[day.date:(day.date + datetime.timedelta(hours=24))]
             day.sentiment = weighted_average(filtered["compound"], filtered["weight"]) 
     return final_dfs, dfs
 def add_weighted_popularity(final_dfs, dfs):
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         for day in df.itertuples():
             filtered = dfs[stock].loc[day.date:(day.date + datetime.timedelta(hours=24))]
             weights = filtered["weight"].sum()
@@ -53,19 +53,18 @@ def add_weighted_popularity(final_dfs, dfs):
             day.volume = ((weights * num_tweets)/24)
     return final_dfs, dfs
 def add_stock_change(final_dfs):
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         for day in df.itertuples():
             afilter = {"date": day.date} 
             day.delta = db[stock].find_one(afilter).change
     return final_dfs
 def get_weighted_data(dfs):
-    print(dfs) 
     dfs = add_weights(dfs)
     dfs = convert_times(dfs)
     final_dfs = dict.fromkeys(db.collection_names(),pd.DataFrame(columns=["date", "sentiment", "volume", "delta"]))
-    for stock, df in dfs:
+    for stock, df in dfs.items():
         df.set_index(["date"])
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         df["date"] = (pd.date_range(start="2014-01-01", end="2016-03-31") + pd.date_range(start="2019-06-01", end="2019-08-31"))
         #final_dfs[stock] = df 
     final_dfs, dfs = add_weighted_sentiment(final_dfs, dfs) 
@@ -74,14 +73,14 @@ def get_weighted_data(dfs):
     return final_dfs
 
 def add_sentiment(final_dfs, dfs):
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         for day in df.itertuples():
             filtered = dfs[stock].loc[day.date:(day.date + datetime.timedelta(hours=24))]
             day.sentiment = np.average(filtered["compound"]) 
     return final_dfs, dfs
 
 def add_popularity(final_dfs, dfs):
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         for day in df.itertuples():
             filtered = dfs[stock].loc[day.date:(day.date + datetime.timedelta(hours=24))]
             num_tweets = filtered.shape[0]
@@ -92,13 +91,13 @@ def add_popularity(final_dfs, dfs):
 def get_unweighted_data(dfs):
     dfs = convert_times(dfs)
     final_dfs = dict.fromkeys(db.collection_names(),pd.DataFrame(columns=["date", "sentiment", "volume", "delta"]))
-    for stock, df in dfs:
+    for stock, df in dfs.items():
         df.set_index(["date"])
-    for stock, df in final_dfs:
+    for stock, df in final_dfs.items():
         df["date"] = (pd.date_range(start="2014-01-01", end="2016-03-31") + pd.date_range(start="2019-06-01", end="2019-08-31"))
     final_dfs, dfs = add_sentiment(final_dfs, dfs) 
     final_dfs, dfs = add_popularity(final_dfs, dfs)
     final_dfs = add_stock_change(final_dfs) 
     return final_dfs
-print(get_weighted_data(get_as_dataframe(["AAPL", "T"])))
+print(get_weighted_data(get_as_dataframe(["AAPL"])))
 #print(get_weighted_data(get_as_dataframe(["AAPL"]))) 
